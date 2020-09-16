effdd8d98e409eb3c48afa55078c40196a385614
johannes117 <51395795+johannes117@users.noreply.github.com>
2020-09-16 14:00:10 +1000

    initial

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {
+    AppBar, 
+    Toolbar, 
+    ListItem, 
+    IconButton,
+    ListItemText, 
+    Avatar, 
+    Divider, 
+    List, 
+    Typography, 
+    Box
+} from '@material-ui/core'
+import {
+    ArrowBack, 
+    AssignmentInd,
+    Home, 
+    Apps, 
+    ContactMail
+} from '@material-ui/icons'
+
+const navbar = () => {
+    return (
+        <Box component = "nav" >
+            <AppBar>
+                <Toolbar>
+                    <ArrowBack>
+                        
+                    </ArrowBack>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+
+export default navbar
