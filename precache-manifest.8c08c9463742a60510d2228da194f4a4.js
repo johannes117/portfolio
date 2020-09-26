79b0273aa1ea54099ef2f918b7aba73048820d0d
johannes117 <51395795+johannes117@users.noreply.github.com>
2020-09-26 18:21:33 +1000

    Updates

---
+++ precache-manifest.8c08c9463742a60510d2228da194f4a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc14b8a86c4b4678b7c288809445cfd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "482fd22000ff1c991756",
+    "url": "/portfolio/static/css/main.2a86ed1c.chunk.css"
+  },
+  {
+    "revision": "be12081ae7e6ab991761",
+    "url": "/portfolio/static/js/2.f3740777.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio/static/js/2.f3740777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482fd22000ff1c991756",
+    "url": "/portfolio/static/js/main.e22ba0c7.chunk.js"
+  },
+  {
+    "revision": "c53d9443373a56138b0b",
+    "url": "/portfolio/static/js/runtime-main.aad75174.js"
+  },
+  {
+    "revision": "09323f5ebf8f7fe5eb0450b9ab832cac",
+    "url": "/portfolio/static/media/applewatch1.09323f5e.jpg"
+  },
+  {
+    "revision": "0d0a996f3748148ce4e508549d421a03",
+    "url": "/portfolio/static/media/circle-cropped.0d0a996f.png"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/portfolio/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/portfolio/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "2c9a38c25fef46fc7987549a9f38d3bc",
+    "url": "/portfolio/static/media/playground-07-desktop.2c9a38c2.png"
+  }
+]);
